0b22f369fa692e91f0bd250de735efd301f9c363
samama-tariq <samama177@gmail.com>
2021-11-12 18:46:03 +0500

    Day 1 complete

---
+++ day_1/project.py
@@ -0,0 +1,10 @@
+#day 1 project Band name generator
+
+print('Welcome to the Band Name Generator')
+
+city_name=input("What's name of city you grew up in?")
+pet_name=input("What's your pet name?")
+band_name=city_name+" "+pet_name
+
+print("Your band name could be"+" "+band_name)
+
